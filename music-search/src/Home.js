754938ff05c36b06e82fbfaf6f1eeee45ad9e7e7
morgan Peck <mlpeck@fullsail.edu>
2017-12-05 23:18:14 -0600

    Starting building my home page
    I have gotten it where you can call the data to build up the home page for the top artists. I am able to loop through and display all the names I just have to work on on displaying all of the pictures

--- music-search/src/Home.js
+++ music-search/src/Home.js
@@ -1,18 +1,50 @@
 import React, {Component} from 'react';
 
-const loadData="ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=41adbd33ca5a768aa7ba9a51e3f747f6&format=json";
+
 
 
 class Home extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            loading: true,
+            artists:[],
+        };
+
+    }
+    componentWillMount() {
+        var array=[]
+        fetch('http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=41adbd33ca5a768aa7ba9a51e3f747f6&format=json&limit=6')
+            .then((Response)=>Response.json())
+            .then((getResponse)=>{
+            for(var i=0; i<getResponse.artists.artist.length;i++){
+                    array.push(getResponse.artists.artist[i])
+                    this.setState({loading:false,artists:array});
+
+
+            }
+
+        });
+
+
+    }
 
     render(){
-     console.log(loadData);
-        return(
+        console.log(this.state.artists)
 
-            <div>
-                <h1>Home</h1>
-            </div>
-        )
+        if(this.state.loading){
+            return <h3>Loading...</h3>
+        }
+            return(<div>
+                <h1>Top Artists</h1>
+                <div>
+                    <img src={this.state.artists.map((elem)=>elem.image[3]["#text"])} alt={this.state.artists.map((elem)=>elem.name)}/><h2>{this.state.artists.map((elem)=>elem.name)}</h2>
+
+                    <h3>{this.state.artists.name}</h3>
+                </div>
+
+            </div>);
+
     }
 
 
